6a172a1a81f739f87d476a93f35ca74c19ba2eb6
MaateusMDS <maateusmania@gmail.com>
2023-08-07 16:29:54 -0300

    exercicio 1/2

---
+++ src/main/java/conjunto_um/Exercicio1.java
@@ -0,0 +1,24 @@
+//        1. Determine qual é a idade que o usuário faz no ano atual. Para isso solicite o seu ano de
+//        nascimento e o ano atual.
+
+package conjunto_um;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+
+        System.out.print("Em qual ano você nasceu?\nR: ");
+        var anoNascimento = entrada.nextInt();
+
+        System.out.print("Qual é o ano atual?\nR: ");
+        var anoAtual = entrada.nextInt();
+
+        var idade = anoAtual - anoNascimento;
+
+        System.out.println("Você irá fazer este ano ou já fez " + idade + " anos.");
+
+        entrada.close();
+    }
+}
---
+++ src/main/java/conjunto_um/Exercicio2.java
@@ -0,0 +1,24 @@
+//2. Calcule e exiba a quantidade de salários mínimos que um determinado funcionário ganha.
+//        Para isto, peça o valor do seu salário e o valor do salário mínimo atual.
+
+package conjunto_um;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        var entrada = new Scanner(System.in);
+
+        System.out.print("Qual o valor do salário mínimo? R:");
+        double salarioMinimo = entrada.nextDouble();
+        System.out.print("Qual o valor do seu salário? R:");
+        double salario = entrada.nextDouble();
+
+        double quantSalario = salario/salarioMinimo;
+
+        String mensagem = (quantSalario == 1.0) ? "salário mínimo" : "salários mínimos";
+
+        System.out.println("Você recebe aproximadamente " + quantSalario + " " + mensagem + ".");
+    }
+}
